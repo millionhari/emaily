c072e3fb6b7adc147eca9284c70b1aff1299eefd
Michael Hari <michaelha@michaelhaMBP15TB.local>
2019-01-06 22:50:28 -0800

    Finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,6 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // record exists
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then(user => done(null, user));
+        }
+      });
+
       console.log('accessToken', accessToken);
       console.log('refreshToken', refreshToken);
       console.log('profile', profile);
